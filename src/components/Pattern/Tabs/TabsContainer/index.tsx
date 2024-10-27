b93dc3c95b13eea9a9f6bed8506a67cbed2af2d6
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-10-27 14:49:12 +0100

    fix: types of Tabs wrapper component

--- src/components/Pattern/Tabs/TabsContainer/index.tsx
+++ src/components/Pattern/Tabs/TabsContainer/index.tsx
@@ -3,12 +3,12 @@ import Tabs from '../../../UI/Tabs';
 import { TabsContextProvider, type TabsContextType } from '../TabsContext';
 import type { TabsContainerProps } from './TabsContainer.types';
 
-export default function TabsContainer({ children, defaultTabId }: TabsContainerProps) {
+export default function TabsContainer({ className, children, defaultTabId }: TabsContainerProps) {
   const [currentTabId, setCurrentTab] = useState<TabsContextType['currentTabId']>(defaultTabId);
 
   return (
     <TabsContextProvider value={{ currentTabId, setCurrentTab }}>
-      <Tabs.Wrapper>{children}</Tabs.Wrapper>
+      <Tabs.Wrapper className={className}>{children}</Tabs.Wrapper>
     </TabsContextProvider>
   );
 }
---
+++ src/components/UI/Tabs/TabWrapper/TabWrapper.types.ts
@@ -0,0 +1,10 @@
+export interface TabWrapperProps {
+  /**
+   * The content of the tab, any (children) element will be rendered on the same row
+   */
+  children: React.ReactNode;
+  /**
+   * Class to personalise the styles
+   */
+  className?: string;
+}
