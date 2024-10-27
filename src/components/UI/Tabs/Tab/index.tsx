43b91617820e717986742f0ca03f72e8b430b238
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-10-27 17:33:52 +0100

    feat: switch tabs with arrow keys

--- src/components/UI/Tabs/Tab/index.tsx
+++ src/components/UI/Tabs/Tab/index.tsx
@@ -5,6 +5,7 @@ import type { TabProps } from './Tab.types';
 export default function Tab({ children, id, selected, variant, ...props }: TabProps) {
   const classes = `${styles.btn} ${styles[variant]}`;
   const { tabId, tabContentId } = getTabIds(id);
+  const tabIndex = selected ? 0 : -1;
 
   return (
     <button
@@ -13,10 +14,33 @@ export default function Tab({ children, id, selected, variant, ...props }: TabPr
       aria-selected={selected}
       className={classes}
       id={tabId}
+      onKeyDown={keydownHandler}
       role="tab"
+      tabIndex={tabIndex}
       type="button"
     >
       {children}
     </button>
   );
+}
+
+function keydownHandler(event: React.KeyboardEvent<HTMLButtonElement>) {
+  if (['ArrowLeft', 'ArrowRight'].includes(event.key)) {
+    const currentTab = event.target as HTMLButtonElement;
+    if (currentTab.parentNode?.childNodes) {
+      const tabs = Array.from(currentTab.parentNode?.childNodes) as HTMLButtonElement[];
+      const index = tabs.findIndex((item) => item.id === currentTab.id);
+      let nextIndex: number | undefined = undefined;
+      if (event.key === 'ArrowLeft') {
+        nextIndex = index - 1;
+      }
+      if (event.key === 'ArrowRight') {
+        nextIndex = index === tabs.length - 1 ? 0 : index + 1;
+      }
+      if (nextIndex != undefined) {
+        const nextTab = tabs.at(nextIndex);
+        nextTab?.focus();
+      }
+    }
+  }
 }
--- src/components/UI/Tabs/Tabs.test.tsx
+++ src/components/UI/Tabs/Tabs.test.tsx
@@ -31,24 +31,54 @@ function TestComponent({ onClick }: { onClick?: () => void }) {
   );
 }
 
-test('should render', () => {
-  render(<TestComponent />);
+describe('Tabs UI', () => {
+  test('should render', () => {
+    render(<TestComponent />);
 
-  expect(screen.getByRole('tablist')).toBeInTheDocument();
-  expect(screen.getAllByRole('tab')).toHaveLength(3);
-  expect(screen.getAllByRole('tabpanel')).toHaveLength(3);
-});
+    expect(screen.getByRole('tablist')).toBeInTheDocument();
+    expect(screen.getAllByRole('tab')).toHaveLength(3);
+    expect(screen.getAllByRole('tabpanel')).toHaveLength(3);
+  });
 
-test('should call the callback when clicking a tab', () => {
-  const onClickSpy = vi.fn();
+  test('should call the callback when clicking a tab', () => {
+    const onClickSpy = vi.fn();
 
-  render(<TestComponent onClick={onClickSpy} />);
+    render(<TestComponent onClick={onClickSpy} />);
 
-  const { tabId } = getTabIds(ids[0]);
-  const tab = screen.getByRole('tab', { name: `Label ${ids[0]}` });
-  expect(tab).toHaveAttribute('id', tabId);
+    const { tabId } = getTabIds(ids[0]);
+    const tab = screen.getByRole('tab', { name: `Label ${ids[0]}` });
+    expect(tab).toHaveAttribute('id', tabId);
 
-  fireEvent.click(tab);
+    fireEvent.click(tab);
 
-  expect(onClickSpy).toHaveBeenCalled();
+    expect(onClickSpy).toHaveBeenCalled();
+  });
+
+  test('should focus another tab per pressing an arrow key', () => {
+    // act
+    render(<TestComponent />);
+    const currentTab = screen.getByRole('tab', { name: `Label ${ids[0]}` });
+    const tabOnRight = screen.getByRole('tab', { name: `Label ${ids[1]}` });
+    const tabOnLeft = screen.getByRole('tab', { name: `Label ${ids[2]}` });
+    // assert
+    expect(currentTab).toHaveAttribute('aria-selected', 'true');
+    expect(tabOnRight).toHaveAttribute('aria-selected', 'false');
+    expect(tabOnRight).not.toHaveFocus();
+    // act
+    fireEvent.keyDown(currentTab, { key: 'ArrowRight' });
+    // assert
+    expect(currentTab).toHaveAttribute('aria-selected', 'true');
+    expect(tabOnRight).toHaveAttribute('aria-selected', 'false');
+    expect(tabOnRight).toHaveFocus();
+    // arrange - restored initial focus
+    fireEvent.focus(currentTab);
+    // act
+    fireEvent.keyDown(currentTab, { key: 'ArrowLeft' });
+    // assert
+    expect(tabOnLeft).toHaveFocus();
+    // act - switching back to currentTab without forcing the focus
+    fireEvent.keyDown(tabOnLeft, { key: 'ArrowRight' });
+    // assert
+    expect(currentTab).toHaveFocus();
+  });
 });
