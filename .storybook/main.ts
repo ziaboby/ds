1fea6c48f95ac32b495442246705532311cf0cb1
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-10-27 17:49:38 +0100

    feat: setup app and Storybook (#1)
    * feat: first import
    
    * doc: upd readme file
    
    * chore: add usefull Storybook addons
    
    * chore: add prettier config
    
    * feat: add font (asset) to both the project and storybook
    
    * fix: format
    
    * styles: initial setup in both app and storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    'storybook-addon-pseudo-states',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+export default config;
